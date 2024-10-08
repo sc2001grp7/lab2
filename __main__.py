a8ed27b369e6084f64e01d34c689c8dddc65ffa7
Elycts Shaders <YoukuShaders@gmail.com>
2024-10-08 17:30:56 +0800

    accelerate using numpy

--- __main__.py
+++ __main__.py
@@ -1,21 +1,24 @@
 import random
 import timeit
 
+import tqdm
+import numpy as np
+
 from parta import dijkstra as dijkstra_matrix_array
 from partb import Dijkstra_ShortestPath as dijkstra_list_heap
 
 def generate_weighted_undirected_graph(n: int, m: int):
     assert n > 0 and m > 0 and m <= n*(n-1)//2
 
-    g_matrix = [[float('inf') for _ in range(n)] for _ in range(n)]
+    edges = np.array(np.triu_indices(n, 1)).T
+    g_matrix = np.full((n, n), float('inf')).tolist()
     g_list = {}
-    for _ in range(m):
-        while True:
-            u = random.randint(0, n-1)
-            v = random.randint(0, n-1)
-            if u != v:
-                break
-        w = random.randint(1, 100)
+
+    print("Generating random graph...")
+    selected_edges = edges[np.random.choice(edges.shape[0], m, replace=False)].tolist()
+    weights = np.random.randint(1, 101, size=m).tolist()
+
+    for (u, v), w in tqdm.tqdm(zip(selected_edges, weights), total=m):
         g_matrix[u][v] = w
         g_matrix[v][u] = w
         if u not in g_list:
@@ -24,15 +27,16 @@ def generate_weighted_undirected_graph(n: int, m: int):
         if v not in g_list:
             g_list[v] = {}
         g_list[v][u] = w
+
     return g_matrix, g_list
 
 if __name__ == "__main__":
     # random.seed(42)
-    n = 10
+    n = 1000
     runs = 10
 
-    # Generate a random sparse graph (m = n)
-    m = n
+    # Generate a random sparse graph (m = n - 1)
+    m = n - 1
     g_matrix, g_list = generate_weighted_undirected_graph(n, m)
     timeit_a = timeit.timeit(lambda: dijkstra_matrix_array(g_matrix, 0), number=runs)
     timeit_b = timeit.timeit(lambda: dijkstra_list_heap(g_list, 0), number=runs)
